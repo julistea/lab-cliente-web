111e4d68e6c6422e2e1f898a4482367678f2f256
Sebastian Sanchez <sasanchezmiles@teco.com.ar>
2024-11-20 20:01:17 -0300

    footer

---
+++ js/features/list-categories.js
@@ -0,0 +1,6 @@
+export const renderCategories = (store) => {
+  const { data: categories } = store.categories;
+  const categoriesList = document.querySelector("#categories-list");
+  for (const category of categories)
+    categoriesList.innerHTML += `<li class="mb-1.5 capitalize">${category}</li>`;
+};
--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "./app/catalog-store.js";
 import { renderCatalog } from "./features/list-catalog.js";
+import { renderCategories } from "./features/list-categories.js";
 
 const init = async () => {
   const store = await createStore().fetchCatalog();
   renderCatalog(store);
+  renderCategories(store);
 };
 
 init();
